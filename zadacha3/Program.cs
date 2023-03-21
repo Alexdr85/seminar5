a7d5734ca50639c10b05c83b92f73b3e7f5ccc7b
Alexandr <dunkelpunk@gmail.com>
2023-03-21 21:31:04 +0300

    seminar zadachi

---
+++ zadacha3/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+
+void InputArray(int[]array)
+{
+    for (int i = 0; i < array.Length; i++)
+     array[i] = new Random().Next(0,10);
+}
+
+string ReleaseArray(int[]array,int k)
+{
+   
+    for (int i = 0; i < array.Length; i++)
+       if (array[i]== k)
+            return "yas";
+    return "no";
+           
+}
+void PrintArray(int[] array)
+{
+    Console.WriteLine($"[{string.Join(",", array)}]");
+}
+
+
+Console.Clear();
+ Console.Write($"n = ");
+ int n = Convert.ToInt32(Console.ReadLine());
+ Console.Write($"k = ");
+ int k = Convert.ToInt32(Console.ReadLine());
+ int[] array = new int[n];
+ InputArray(array);
+ PrintArray(array);
+ Console.WriteLine(ReleaseArray(array, k));
+  
---
+++ zadacha5/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+
+
+
+void InputArray(int[]array)
+{
+    for (int i = 0; i < array.Length; i++)
+     array[i] = new Random().Next(1,10);
+}
+
+void ReleaseArray(int[]array)
+{
+   
+    for (int i = 0; i < array.Length / 2 + array.Length % 2 ; i++)
+       Console.Write($"{array[i]*array[array.Length - 1 - i]}");
+    
+           
+}
+void PrintArray(int[] array)
+{
+    Console.WriteLine($"[{string.Join(",", array)}]");
+}
+
+
+Console.Clear();
+ Console.Write($"n = ");
+ int n = Convert.ToInt32(Console.ReadLine());
+ int[] array = new int[n];
+ InputArray(array);
+ PrintArray(array);
+ ReleaseArray(array);
+ 
